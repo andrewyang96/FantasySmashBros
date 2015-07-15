740cd7c66643a97b142879124cebde844f05de4c
Andrew Yang <ayang14@illinois.edu>
2015-07-14 21:26:06 -0500

    Modify popularity gathering script to include numParticipants

--- popular/popular.py
+++ popular/popular.py
@@ -9,7 +9,7 @@ DATAFILEPATH = os.path.abspath(os.path.join(CURDIR, "..", "data", "{0}.json"))
 OUTFILEPATH = os.path.join(CURDIR, "popular.json")
 GAMES = ["ssb4", "ssbm"]
 
-def getFreqs(game, limit=None):
+def getFreqsAndParticipants(game, limit=None):
     print "Fetching players for game", game
     participants = ref.get("/{0}/participants".format(game), None)
     numParticipants = float(len(participants))
@@ -18,9 +18,9 @@ def getFreqs(game, limit=None):
     freqs = sorted([(ID, 100 * len(players) / numParticipants) for ID, players in res.iteritems()], key=lambda x: -x[1])
     print "Returning frequencies"
     if limit is None or limit <= 0:
-        return freqs
+        return (freqs, numParticipants)
     else:
-        return freqs[:limit]
+        return (freqs[:limit], numParticipants)
 
 def getPlayers(game):
     print "Getting player list for game", game
@@ -28,14 +28,14 @@ def getPlayers(game):
         return json.load(f)
 
 def combineFreqsAndPlayers(game, limit=None):
-    freqs = getFreqs(game, limit)
+    freqs, numParticipants = getFreqsAndParticipants(game, limit)
     players = getPlayers(game)
     ret = []
     for freq in freqs:
         newObj = {"id": freq[0], "popularity": round(freq[1], 2)}
         newObj.update(players[freq[0]])
         ret.append(newObj)
-    return ret
+    return {"freqs": ret, "numParticipants": numParticipants}
 
 def main():
     ret = {"lastUpdated": str(datetime.datetime.now())}
