8b4966c3ec14e940d51c35c6794f1cdab73b9644
Andrew Yang <ayang14@illinois.edu>
2015-07-16 23:07:39 -0500

    Functions for popular.js

--- js/popular.js
+++ js/popular.js
@@ -1,9 +1,20 @@
-$.ajax({
-	type: "GET",
-	dataType: "json",
-	async: true,
-	url: "../popular/popular.json",
-	success: function (data) {
-		console.log(data);
-	}
+var getSmasherPopularity = function (callback) {
+	$.getJSON("http://fantasysmashbros.com/popular/popular.json", callback);
+};
+
+var renderPopularity = function (data) {
+	// TODO
+};
+
+var getStandings = function (callback) {
+	// TODO
+};
+
+var renderStandings = function (data) {
+	// TODO
+};
+
+$(document).ready(function () {
+	attemptLogin();
+	getSmasherPopularity(renderPopularity);
 });
