1bffe8f1a8cf528fa9b5c38c9531be0c6cf00f7f
Andrew Yang <andrewyang96@gmail.com>
2015-12-30 03:02:11 -0600

    Update calculator client JS code

--- client/public/javascripts/calculator.js
+++ client/public/javascripts/calculator.js
@@ -1,17 +1,33 @@
+var PLACES = [1,2,3,4,5,7,9,13,17,25];
+
 $("#calculator").submit(function (event) {
     // Calculate score
     var percent = $("input[name=percent]").val();
-    var place = $("input[name=place]").val();
-    var score = calculateScore(percent, place);
-    $("#score").html(score);
+    var proportion = percent / 100;
+    var place = $("input[name=place]").val();    
     
     // Render score spread
     $("#percent").html(percent);
-    var tbody = $("#scoreSpread > tbody");
-    tbody.html("");
-    calculateScoreSpread(percent).forEach(function (obj) {
-        var html = "<tr><td>"+obj.place+"</td><td>"+obj.score+"</td></tr>";
-        tbody.append(html);
+    
+    $.get("http://localhost:4747/scoring/scorespread?proportion=" + proportion, function (data) {
+        var spread = JSON.parse(data).scoreSpread;
+
+        if (place <= 32) {
+            for (var i = 0; i < PLACES.length; i++) {
+                if (PLACES[i] >= place) {
+                    $("#score").html(spread[i]);
+                    break;
+                }
+            }
+        } else {
+            $("#score").html(0);
+        }
+
+        var tbody = $("#scoreSpread > tbody");
+        tbody.html("");
+        for (var i = 0; i < PLACES.length; i++) {
+            var html = "<tr><td>" + PLACES[i] + "</td><td>" + spread[i] + "</td></tr>";
+            tbody.append(html);
+        }
     });
-    console.log("done");
 });
